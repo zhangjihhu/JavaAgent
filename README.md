d2fafbb74e07464d82dfa6d2538f8d2e0489506f
zhangji-hhu <1325626881@qq.com>
2019-09-16 10:33:31 +0800

    fix README.md

--- README.md
+++ README.md
@@ -2,3 +2,42 @@
 use javaagent modify jvm class
 
 from jdk5, jdk import java.lang.instrument, we can modify class file throw running premain method when jvm load class.
+
+* instrumentation
+  * 静态instrumentation
+    * 通过添加agent代理，可以对jvm加载前的class文件动态修改
+  * 动态instrumentation
+    * 通过Attach API，与jvm启动后的进程通信，进而同台修改class文件
+
+
+###demo1:
+使用静态instrumentation， 在jvm启动时添加一个代理(javaagent)，每个代理是一个jar包，代理类包含一个premain方法，premain方法可以对加载前的class文件进行修改，无需对原有应用做任何修改，就可以实现类的动态修改和增强
+```
+instrumentation.addTransformer()
+
+java -javaagent:agent.jar MyClass
+```
+
+###demo2:
+使用动态instrumentation，在jvm启动后通过Attach API远程加载和使用，实现对class文件的修改
+1. 编写Attach Agent(打包成agent.jar)(如方法一)
+    ```
+    instrumentation.retransformClasses()
+    ```
+2. 使用VirtualMachine.attach()方法和目标jvm进程跨进程通信
+    ```
+    VirtualMachine vm = VirtualMachine.attach(args[0]);
+    vm.loadAgent("F:\\Project\\java\\javaagent\\target\\my-javaagent.jar");
+    vm.detach();
+    ```
+3. 启动目标jvm进程
+    ```
+    java MyClass
+    ```
+4. attach到目标进程
+    ```
+    java MyAttachMain pid(pid为目标进程id)
+    ```
+
+###demo3
+使用javaagent破解Censum
--- src/main/java/com/hhu/demo2/MyAttachMain.java
+++ src/main/java/com/hhu/demo2/MyAttachMain.java
@@ -1,21 +1,21 @@
 package com.hhu.demo2;
 
-// import com.sun.tools.attach.AgentInitializationException;
-// import com.sun.tools.attach.AgentLoadException;
-// import com.sun.tools.attach.AttachNotSupportedException;
-// import com.sun.tools.attach.VirtualMachine;
-//
-// import java.io.IOException;
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+
+import java.io.IOException;
 
 
 public class MyAttachMain {
 
-	// public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
-	// 	System.out.println("MyAttachMain");
-	// 	VirtualMachine vm = VirtualMachine.attach(args[0]);
-	// 	vm.loadAgent("F:\\Project\\java\\javaagent\\target\\my-javaagent.jar");
-	// 	vm.detach();
-	//
-	// }
+	public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
+		System.out.println("MyAttachMain");
+		VirtualMachine vm = VirtualMachine.attach(args[0]);
+		vm.loadAgent("F:\\Project\\java\\javaagent\\target\\my-javaagent.jar");
+		vm.detach();
+
+	}
 
 }
